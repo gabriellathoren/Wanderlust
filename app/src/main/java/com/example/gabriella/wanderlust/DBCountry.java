f1e8d044f9053795f889a854d8c5e91ffb9005b5
gabriellathoren <gabriella_thoren@hotmail.com>
2016-01-07 15:18:12 +0100

    Copyright

--- app/src/main/java/com/example/gabriella/wanderlust/DBCountry.java
+++ app/src/main/java/com/example/gabriella/wanderlust/DBCountry.java
@@ -10,6 +10,24 @@ package com.example.gabriella.wanderlust;
  * @author  Gabriella Thorén
  * @version 1
  *
+ *
+ * Wanderlust - the application that keeps track of your travels.
+ * Copyright (C) 2016 Gabriella Thorén
+ * <p>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
  */
 public class DBCountry {
 
